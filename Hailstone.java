bd14c49f49bab893e9caa54589dba7b0bd08f283
Brendan Ng <brendanng88@gmail.com>
2025-10-30 12:04:49 -0400

    Update Hailstone.java

--- Hailstone.java
+++ Hailstone.java
@@ -1,29 +1,22 @@
-//Brendan Ng
 import java.util.Scanner;
-public class Hailstone {
+
+public class hailstone {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Initial: ");
-        int initial = scanner.nextInt();
-        int amt = 0;
-        for (int i = 200; i <= 1; i--) {
-            while (true) {
-                if (initial == 4) {
+
+        for (int i = 200; i >= 1; i--) {
+            while (true) { 
+                if (i == 1) {
                     break;
                 }
-                else if (initial % 2 == 0) {
-                    initial = (int) initial / 2;
+                if (i % 2 == 0) {
+                    i /= 2;
                 }
                 else {
-                    initial = (initial * 3) + 1;
+                    i = (i * 3) + 1;
                 }
-                System.out.println(initial);
-                amt++;
-            } 
-            System.out.println("Amount of processes: "+amt);
+            }
         }
         scanner.close();
     }
-
-
 }
